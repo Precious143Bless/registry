d3dd81ac6e70a92241193d86a5ec8785774302dd
roxasangeline09-cmyk <roxasangeline09@gmail.com>
2026-03-25 16:17:29 +0800

    Assets video
    for assets video

--- church_registry/settings.py
+++ church_registry/settings.py
@@ -79,7 +79,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'registry' / 'templates' / 'registry' / 'assets',
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
