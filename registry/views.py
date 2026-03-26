6c3d133e35297de94dff9eea5b79c4913cf1d473
roxasangeline09-cmyk <roxasangeline09@gmail.com>
2026-03-26 08:35:38 +0800

    prof

--- registry/views.py
+++ registry/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib import messages
 from django.db.models import Q
 from .models import Member, Baptism, Confirmation, FirstHolyCommunion, Marriage, LastRites, Pledge, PledgePayment
 from .forms import (MemberForm, BaptismForm, ConfirmationForm, CommunionForm,
-                    MarriageForm, LastRitesForm, PledgeForm, PledgePaymentForm)
+                    MarriageForm, LastRitesForm, PledgeForm, PledgePaymentForm, ProfileForm)
 
 
 # ─── AUTH ────────────────────────────────────────────────────────────────────
